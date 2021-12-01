e11fe12a44f9fb6bea220fd9ce4bb5a5c1b6a972
Eduardo913 <cesarvilla903@gmail.com>
2021-12-01 17:33:29 -0600

    cambios

--- Main.ino
+++ Main.ino
@@ -9,7 +9,6 @@
 #include <FirebaseESP8266.h>
 #include <FirebaseFS.h>
 #include <Utils.h>
-#include <Ticker.h>
 
 const char *FIREBASE_HOST = "iot-eduardo-default-rtdb.firebaseio.com";
 const char *FIREBASE_AUTH = "aEcmZDKckfRM3riyIJ7FPjYKwTJ0QZtz1RNWkBTV";
@@ -18,21 +17,17 @@ String path = "/MyHome";
 
 FirebaseData firebaseData;
 
-boolean BoolVerificar = false;
-
 #define LED1 D2
 
 int EstadoLed;
 
-
-Ticker timer;
 
 void setup() {
     Serial.begin(115200);
     // pines a usar 
     pinMode(LED1,OUTPUT);
     EstadoLed = 0;
-    timer.attach(4,funcion_1);
+
     // conexion a internet
     initConnection(); //WifiScript
 
@@ -42,27 +37,35 @@ void setup() {
     Firebase.begin(FIREBASE_HOST,FIREBASE_AUTH);
     Firebase.reconnectWiFi(true);
 
+    Firebase.setStreamCallback(firebaseData, streamCallback, streamTimeoutCallback);
+    if (!Firebase.beginStream(firebaseData, path + "/Dispositivo/Luz_1")){
+      Serial.println(firebaseData.errorReason());
+    }
+
+
 }
 
 void loop() {
 
-  if(BoolVerificar == true){
-    BoolVerificar = false;
-    if(Firebase.getInt(firebaseData,path + "/Dispositivo/Luz_1")){
-      int data = firebaseData.to<int>();
-      if(data != EstadoLed){
-        Serial.println("el valor del led a cambiado");
-        EstadoLed = data;
-        digitalWrite(LED1,EstadoLed);
-      }
-    }else{
-      Serial.println("no es un entero");
-    }
-    
-  }
-  
 }
 
-void funcion_1(){
-  BoolVerificar = true;
+void streamCallback(StreamData data){
+  
+  if (data.dataTypeEnum() == fb_esp_rtdb_data_type_integer){
+    int valor = data.to<int>();
+      if(valor != EstadoLed){
+        Serial.println("el valor del led a cambiado");
+        EstadoLed = valor;
+        digitalWrite(LED1,EstadoLed);
+      }
+  }
+}
+
+
+void streamTimeoutCallback(bool timeout)
+{
+  if(timeout){
+    //Stream timeout occurred
+    Serial.println("Stream timeout, resume streaming...");
+  }
 }
